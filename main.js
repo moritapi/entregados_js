ad62d1aa0cb8c69eee7507077c418901a8634419
moritapi <moritapi@gmail.com>
2022-10-11 20:37:00 -0500

    entrega número dos

---
+++ main.js
@@ -0,0 +1,35 @@
+let saludo = alert("Bienvenido a HappyPet, ingresa tu busqueda a través de la consola")
+
+const veterinaria=[
+
+];
+
+class articulos{
+    constructor(animales, accesorios, comida) {
+     this.nombre = nombre;
+     this.producto = producto;
+    }
+   }
+
+const animales = [
+    {id: 1, nombre:"Jacobo", raza: "Chiguagua"},
+    {id: 2, nombre:"Oregano", raza: "Gato Siberiano"},
+    {id: 3, nombre:"Candela", raza: "BullDog"},
+    {id: 4, nombre:"Violeta", raza: "Gato Persa"},
+    {id: 5, nombre:"Jake", raza: "Alaska"},
+    ];
+
+const accesorios = [
+    {id: 1, nombre:"juguetes"},
+    {id: 2, nombre:"correas"},
+    {id: 3, nombre:"camas"},
+    {id: 4, nombre:"arena"},
+    {id: 5, nombre:"platos"},
+    ];
+
+const comida = [
+    {id: 1, nombre:"wiskas"},
+    {id: 2, nombre:"pettiDog"},
+    ];
+    
+    console.log(veterinaria);
