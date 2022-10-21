6ba26ca62aeca78318ea8e2ce5a7b2551551a91f
moritapi <moritapi@gmail.com>
2022-10-21 15:08:08 -0500

    elabore el array e ingrese metodos de busqueda

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
-let saludo = alert("Bienvenido a HappyPet, ingresa tu busqueda a través de la consola")
 
-const veterinaria=[
+let usuario= prompt("ingresa tu nombre");
+    console.log("Bienvenido a HappyPet, ingresa tu busqueda de animales" + usuario);
 
-];
-
-class articulos{
-    constructor(animales, accesorios, comida) {
-     this.nombre = nombre;
-     this.producto = producto;
-    }
-   }
-
-const animales = [
-    {id: 1, nombre:"Jacobo", raza: "Chiguagua"},
-    {id: 2, nombre:"Oregano", raza: "Gato Siberiano"},
-    {id: 3, nombre:"Candela", raza: "BullDog"},
-    {id: 4, nombre:"Violeta", raza: "Gato Persa"},
-    {id: 5, nombre:"Jake", raza: "Alaska"},
-    ];
-
-const accesorios = [
-    {id: 1, nombre:"juguetes"},
-    {id: 2, nombre:"correas"},
-    {id: 3, nombre:"camas"},
-    {id: 4, nombre:"arena"},
-    {id: 5, nombre:"platos"},
-    ];
-
-const comida = [
-    {id: 1, nombre:"wiskas"},
-    {id: 2, nombre:"pettiDog"},
-    ];
+let animales = [
+        {id: 1, nombre:"Jacobo", raza: "Chiguagua"},
+        {id: 2, nombre:"Oregano", raza: "Gato Siberiano"},
+        {id: 3, nombre:"Candela", raza: "BullDog"},
+        {id: 4, nombre:"Violeta", raza: "Gato Persa"},
+        {id: 5, nombre:"Jake", raza: "Alaska"},
     
-    console.log(veterinaria);
+    ]
+
+const buscar = animales.find(animales=> animales.id === 3)
+    console.log(buscar)
+
+const ingreso = animales.some(animales => animales.nombre === "Jacobo")
+    console.log(ingreso ) 
+    
+const razas = animales.map(animales => animales.raza)
+    console.log(razas);
+    
+    
